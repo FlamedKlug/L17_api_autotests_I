1c7fb66c6130162f49ced046210e750463218743
Anton Maloivan <kluge@bk.ru>
2025-11-16 08:32:13 +0300

    work on mistakes replace asserts in test_method_response_body_schema_post_users
    and 2 new test:
    - no body
    - body == empty json

--- test_requests.py
+++ test_requests.py
@@ -1,3 +1,4 @@
+import pytest
 from jsonschema import validate
 import requests
 import schemas
@@ -24,8 +25,9 @@ def test_method_response_body_schema_post_users():
                                  "name": "morpheus",
                                  "job": "leader"
                              })
-    validate(response.json(), schema=schemas.response_post_users_ok)
+    # Переставил местами. Не совсем понятно в чем отличие - почему надо сначала проверять статус-код, а потом схему?
     assert response.status_code == 201
+    validate(response.json(), schema=schemas.response_post_users_ok)
 
 
 def test_method_response_status_delete_users():
@@ -79,3 +81,34 @@ def test_method_response_schema_patch_users():
                               })
     assert response.status_code == 200
     validate(response.json(), schema=schemas.response_patch_users_ok)
+
+
+"""
+        С точки зрения тестирования метода без ответа, то у меня был тест test_method_response_status_delete_users
+    Думал его достаточно. Правда я там проверял только статус-код и отсутствие ответа - не проверял.
+    На лекции вопрос проверки отсутствия ответа я как-то не припомню. Поэтому посмотрел как этот вопрос решили 
+    другие ученики. Их версия мне показалась не соответствующей условиям - у них проверка идет на ответ состоящий
+    из пустого json = {}. Но это же не соответствует условию. Пустой json в ответе == ответ не пустой. На мой взгляд 
+    надо брать метод у которого в ответе body вообще пустое и проверять это. Согласно тз.
+    Поэтому я сделал тест test_method_response_no_body_delete_users. 
+    
+        Не уверен, что это оптимальное решение, но что смог придумать. Если подскажите лучше вариант - буду благодарен.
+    
+        Ну и на всякий случай, что бы три раза не вставать - приделал тест 
+    test_method_response_empty_json_in_body_get_unknown с проверкой на пустой json в ответе
+"""
+
+
+def test_method_response_no_body_delete_users():
+    response = requests.delete(base_url + endpoint_users + "/2",
+                               headers=header_auth)
+    assert response.status_code == 204
+    with pytest.raises(ValueError):
+        data = response.json()
+
+
+def test_method_response_empty_json_in_body_get_unknown():
+    response = requests.get(base_url + endpoint_unknown + "/23",
+                            headers=header_auth)
+    assert response.status_code == 404
+    assert response.json() == {}
